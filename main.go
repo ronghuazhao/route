9f9c2245df2282a0de8b558f3c446053d23e74e6
Ben Demaree <bendemaree@toadad.com>
2013-12-20 13:34:52 -0600

    Formatting fixes.

--- main.go
+++ main.go
@@ -1,72 +1,72 @@
 package main
 
 import (
-    "code.google.com/p/gcfg"
-    "github.com/Shopify/sarama"
-    "net/http"
-    "net/http/httputil"
-    "net/url"
-    "runtime"
+	"code.google.com/p/gcfg"
+	"github.com/Shopify/sarama"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"runtime"
 )
 
 // Create global log instance
 var log *sarama.Producer
 
 func Log(topic string, key string, value string) {
-    log.QueueMessage(topic, sarama.StringEncoder(key), sarama.StringEncoder(value))
+	log.QueueMessage(topic, sarama.StringEncoder(key), sarama.StringEncoder(value))
 }
 
 func main() {
 
-    // Use all cores
-    runtime.GOMAXPROCS(runtime.NumCPU())
+	// Use all cores
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
-    // Create Kafka connection
-    kafka, err := sarama.NewClient("broadcast", []string{"localhost:9092"}, nil)
-    if err != nil {
-        panic(err)
-    }
-    defer kafka.Close()
+	// Create Kafka connection
+	kafka, err := sarama.NewClient("broadcast", []string{"localhost:9092"}, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer kafka.Close()
 
-    producer, err := sarama.NewProducer(kafka, &sarama.ProducerConfig{
-                RequiredAcks: sarama.WaitForLocal,
-                MaxBufferedBytes: 4096,
-                MaxBufferTime: 4096})
-    if err != nil {
-        panic(err)
-    }
-    defer producer.Close()
+	producer, err := sarama.NewProducer(kafka, &sarama.ProducerConfig{
+		RequiredAcks:     sarama.WaitForLocal,
+		MaxBufferedBytes: 4096,
+		MaxBufferTime:    4096})
+	if err != nil {
+		panic(err)
+	}
+	defer producer.Close()
 
-    // Bind logger globally
-    log = producer
+	// Bind logger globally
+	log = producer
 
-    // Host config file structure
-    type Config struct {
-        Host map[string]*struct {
-            Label string
-        }
-    }
+	// Host config file structure
+	type Config struct {
+		Host map[string]*struct {
+			Label string
+		}
+	}
 
-    // Read in host file
-    var hosts Config
-    gcfg.ReadFileInto(&hosts, "hosts.conf")
+	// Read in host file
+	var hosts Config
+	gcfg.ReadFileInto(&hosts, "hosts.conf")
 
-    mux := NewMux()
+	mux := NewMux()
 
-    // Create route handlers
-    for host, conf := range hosts.Host {
-        url, _ := url.Parse(host)
+	// Create route handlers
+	for host, conf := range hosts.Host {
+		url, _ := url.Parse(host)
 
-        domain := url.Host
-        label := conf.Label
+		domain := url.Host
+		label := conf.Label
 
-        proxy := httputil.NewSingleHostReverseProxy(url)
-        prefix := "/" + label
+		proxy := httputil.NewSingleHostReverseProxy(url)
+		prefix := "/" + label
 
-        mux.Register(label, domain, prefix, proxy)
-    }
+		mux.Register(label, domain, prefix, proxy)
+	}
 
-    Log("internal", "route.start", "Router started")
+	Log("internal", "route.start", "Router started")
 
-    http.ListenAndServe(":9343", mux)
+	http.ListenAndServe(":9343", mux)
 }
--- mux.go
+++ mux.go
@@ -1,61 +1,61 @@
 package main
 
 import (
-    "net/http"
-    "strings"
-    "sync"
+	"net/http"
+	"strings"
+	"sync"
 )
 
 type Mux struct {
-    mu          sync.RWMutex
-    hosts       map[string]Host
+	mu    sync.RWMutex
+	hosts map[string]Host
 }
 
 type Host struct {
-    domain      string
-    handler     http.Handler
+	domain  string
+	handler http.Handler
 }
 
 func NewMux() *Mux {
-    return &Mux{hosts: make(map[string]Host)}
+	return &Mux{hosts: make(map[string]Host)}
 }
 
 func (mux *Mux) Register(label string, domain string, prefix string, handler http.Handler) {
-    // Key in a host by its label
-    mux.hosts[label] = Host{domain: domain, handler: handler}
+	// Key in a host by its label
+	mux.hosts[label] = Host{domain: domain, handler: handler}
 }
 
 func (mux *Mux) Lookup(path string) (host Host) {
-    mux.mu.RLock()
-    defer mux.mu.RUnlock()
+	mux.mu.RLock()
+	defer mux.mu.RUnlock()
 
-    // Extract the prefix from the given path
-    split := strings.Split(path, "/")
-    prefix := split[1]
+	// Extract the prefix from the given path
+	split := strings.Split(path, "/")
+	prefix := split[1]
 
-    // Find the host from its prefix
-    host = mux.hosts[prefix]
+	// Find the host from its prefix
+	host = mux.hosts[prefix]
 
-    return host
+	return host
 }
 
 func (mux *Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    // Fetch host by the given path
-    host := mux.Lookup(r.URL.Path)
+	// Fetch host by the given path
+	host := mux.Lookup(r.URL.Path)
 
-    // Build new path removing prefix
-    split := strings.Split(r.URL.Path, "/")
-    r.URL.Path = "/" + strings.Join(split[2:], "/")
+	// Build new path removing prefix
+	split := strings.Split(r.URL.Path, "/")
+	r.URL.Path = "/" + strings.Join(split[2:], "/")
 
-    // Assign target host header
-    r.Host = host.domain
+	// Assign target host header
+	r.Host = host.domain
 
-    // Assign handler
-    handler := host.handler
+	// Assign handler
+	handler := host.handler
 
-    // Send event to central log
-    Log("route", "request.start", r.Method + " " + r.Host + r.URL.String())
+	// Send event to central log
+	Log("route", "request.start", r.Method+" "+r.Host+r.URL.String())
 
-    // Serve request
-    handler.ServeHTTP(w, r)
+	// Serve request
+	handler.ServeHTTP(w, r)
 }
