e55438b6b7132a876b468c540a9c64430317a0d3
Ben Demaree <bendemaree@toadad.com>
2013-12-22 10:11:32 -0600

    Add tests for router.

---
+++ router_test.go
@@ -0,0 +1,65 @@
+package route
+
+import (
+    "testing"
+	"net/url"
+	"net/http/httputil"
+)
+
+type Registrant struct {
+    label string
+    domain string
+    path string
+    prefix string
+    lookup string
+}
+
+var registrants = []Registrant{
+    {
+        label: "foo",
+        domain: "foo.bar.baz",
+        path: "foo.bar.baz/test",
+        prefix: "/foo",
+        lookup: "/foo/test/abc",
+    },
+}
+
+func register(router *Router) {
+    for _, r := range registrants {
+		url, _ := url.Parse(r.path)
+		proxy := httputil.NewSingleHostReverseProxy(url)
+
+        router.Register(r.label, r.domain, r.prefix, proxy)
+    }
+}
+
+func TestNew(t *testing.T) {
+    router := NewRouter()
+    if len(router.hosts) > 0 {
+        t.Error("A new router should not contain any entries")
+    }
+}
+
+func TestRegister(t *testing.T) {
+    router := NewRouter()
+
+    register(router)
+
+    if len(router.hosts) != len(registrants) {
+        t.Error("Router did not register all hosts")
+    }
+}
+
+func TestLookup(t *testing.T) {
+    router := NewRouter()
+
+    register(router)
+
+    for _, r := range registrants {
+        host := router.Lookup(r.lookup)
+
+        if host.domain != r.domain {
+            t.Errorf("Lookup failed for %s, returned %s", r.lookup, host.domain)
+        }
+    }
+}
