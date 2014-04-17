f87d1a0a13b516b242d734746367e1002b282f24
Trevor Riles <trevorriles@gmail.com>
2014-04-17 15:58:37 -0500

    added all this fun stuff that works with the store service I am about to push

--- route.go
+++ route.go
@@ -1,49 +1,110 @@
 package main
 
 import (
-	"github.umn.edu/umnapi/route.git/logger"
-	"github.umn.edu/umnapi/route.git/router"
-	_ "github.com/mattn/go-sqlite3"
-	"github.com/jmoiron/sqlx"
-	"net/http"
-	"runtime"
+    "bytes"
+    "github.umn.edu/umnapi/route.git/logger"
+    "github.umn.edu/umnapi/route.git/router"
+    "github.umn.edu/umnapi/route.git/interfaces"
+    "code.google.com/p/goprotobuf/proto"
+    "net/http"
+    "net/http/httputil"
+    "net/url"
+    "code.google.com/p/gcfg"
+    "runtime"
+    "github.com/garyburd/redigo/redis"
+    "fmt"
+    zmq "github.com/alecthomas/gozmq"
 )
 
 // Host config file structure
 type Config struct {
-	Host map[string]*struct {
-		Label string
-	}
+    Host map[string]*struct {
+	Label string
+    }
 }
 
 var logging *logger.Logger
 var routing *router.Router
-var keyStore *sqlx.DB
-var routeStore *sqlx.DB
+var store redis.Conn
 
 func init() {
     // Initiate logger
-	keyStore, _ = sqlx.Connect("sqlite3", "/Users/ben/Code/api-auth/db/development.sqlite3")
-	routeStore, _ = sqlx.Connect("sqlite3", "/Users/ben/Code/api-manage/db/development.sqlite3")
+    store, _ = redis.Dial("tcp", ":6379")
     logging = logger.NewLogger("route", logger.Console)
-	routing = router.NewRouter(keyStore, routeStore)
+    routing = router.NewRouter(store, logging)
 }
 
 func main() {
 
-	// Use all cores
-	runtime.GOMAXPROCS(runtime.NumCPU())
+    // Use all cores
+    runtime.GOMAXPROCS(runtime.NumCPU())
 
-	// Create API handler
-	api := NewApi("/api/v1", routing)
+    // Create API handler
+    api := NewApi("/api/v1", routing)
+    // Read in host file
+    var hosts Config
+    gcfg.ReadFileInto(&hosts, "hosts.conf")
+
+    // Create route handlers
+    for host, conf := range hosts.Host {
+	url, _ := url.Parse(host)
+
+	domain := url.Host
+	label := conf.Label
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	prefix := "/" + label
+	path := url.String()
+
+	routing.Register(label, domain, path, prefix, proxy)
+    }
+
+    go zmqListen(store)
 
     // Start router
-	go http.ListenAndServe(":8080", routing)
-	logging.Log("internal", "route.start", "router started", "[fg-blue]")
+    go http.ListenAndServe(":8080", routing)
+    logging.Log("internal", "route.start", "router started", "[fg-blue]")
 
     // Start router API
-	go http.ListenAndServe(":8081", api)
-	logging.Log("internal", "route.start", "api started", "[fg-blue]")
+    go http.ListenAndServe(":8081", api)
+    logging.Log("internal", "route.start", "api started", "[fg-blue]")
 
-	<-make(chan int)
+    <-make(chan int)
+}
+
+func zmqListen(store redis.Conn) {
+    println("started listening on 6666")
+    ctx, _ := zmq.NewContext()
+    sock, _ := ctx.NewSocket(zmq.SUB)
+    sock.Connect("tcp://localhost:6666")
+    defer sock.Close()
+    sock.SetSubscribe("auth")
+    sock.SetSubscribe("route")
+    for {
+	message, _ := sock.RecvMultipart(0)
+
+	topic := message[0]
+	raw := message[1]
+
+	err := error(nil);
+
+	if bytes.Equal(topic, []byte("auth")) {
+	    //store auth
+	    data := &interfaces.Auth{}
+	    err = proto.Unmarshal(raw, data)
+	    // 2) store in appropriate colleciton/table based on topic
+	    logging.Log("internal", "route.new_keypair", fmt.Sprintf("{%s}", data), "[fg-green]")
+	    public_key := data.GetPublicKey()
+	    private_key := data.GetPrivateKey()
+	    redis_key := fmt.Sprintf("key:%s", public_key)
+	    println(redis_key)
+	    store.Do("set", redis_key, private_key)
+	    test, _ := redis.String(store.Do("get", redis_key))
+	    println(test)
+	}
+
+	if err != nil {
+	    println("demarshalling error")
+	}
+    }
 }
--- router/router.go
+++ router/router.go
@@ -1,14 +1,18 @@
 package router
 
 import (
-	"errors"
-	"fmt"
-	"net/http"
-	"strings"
-	"github.com/jmoiron/sqlx"
-	"sync"
-	"net/url"
-	"net/http/httputil"
+    "errors"
+    "fmt"
+    "net/http"
+    "strings"
+    "sync"
+    "net/url"
+    "net/http/httputil"
+    "github.com/garyburd/redigo/redis"
+    zmq "github.com/alecthomas/gozmq"
+    "code.google.com/p/goprotobuf/proto"
+    "github.umn.edu/umnapi/route.git/interfaces"
+    "github.umn.edu/umnapi/route.git/logger"
 )
 
 type Route struct {
@@ -18,70 +22,104 @@ type Route struct {
 }
 
 type Router struct {
-	mu          sync.RWMutex
-	Hosts       map[string]Host
-	keyStore    *sqlx.DB
-	routeStore  *sqlx.DB
+    mu          sync.RWMutex
+    Hosts       map[string]Host
+    store       redis.Conn
+    logging     *logger.Logger
 }
 
 type Host struct {
-	Domain  string          `json:"domain"`
-	Label   string          `json:"label"`
-	Path    string          `json:"path"`
-	Prefix  string          `json:"prefix"`
-	handler http.Handler
+    Domain  string          `json:"domain"`
+    Label   string          `json:"label"`
+    Path    string          `json:"path"`
+    Prefix  string          `json:"prefix"`
+    handler http.Handler
 }
 
-func NewRouter(keyStore *sqlx.DB, routeStore *sqlx.DB) *Router {
-	return &Router{
-		Hosts:  make(map[string]Host),
-		keyStore: keyStore,
-        routeStore: routeStore,
-	}
-}
-
-func (router *Router) Register(label string, domain string, path string, prefix string, handler http.Handler) {
-	// Key in a host by its label
-	router.Hosts[label] = Host{
-        Domain: domain,
-        Label: label,
-        Path: path,
-        Prefix: prefix,
-        handler: handler,
+func NewRouter(store redis.Conn, logging *logger.Logger) *Router {
+    return &Router{
+	Hosts:  make(map[string]Host),
+	store: store,
+	logging: logging,
     }
 }
 
+func (router *Router) Register(label string, domain string, path string, prefix string, handler http.Handler) {
+    // Key in a host by its label
+    router.Hosts[label] = Host{
+	Domain: domain,
+	Label: label,
+	Path: path,
+	Prefix: prefix,
+	handler: handler,
+    }
+
+    // Create zmq context
+    ctx, _ := zmq.NewContext()
+    defer ctx.Close()
+
+    // Create and open zmq socket
+    sock, _ := ctx.NewSocket(zmq.REQ)
+    sock.Connect("tcp://127.0.0.1:6667")
+    defer sock.Close()
+
+    store := router.store;
+
+    // store using the label as the key since that is what we use to build our urls.
+    redis_key := fmt.Sprintf("route:%s", label)
+    store.Do("HMSET", redis_key, "label", redis_key, "domain", domain, "path", path, "prefix", prefix)
+
+    // Create protobuf object
+    message := &interfaces.Route {
+	Do: interfaces.DO_UPDATE.Enum(),
+	Id: proto.String("0"),
+	Label: proto.String(label),
+	Path: proto.String(path),
+	Prefix: proto.String(prefix),
+	Domain: proto.String(domain),
+    }
+
+    data, err := proto.Marshal(message)
+    if err != nil {
+    	router.logging.Log("internal", "router.register", "unable to marshal message", "[fg-red]")
+    	return
+    }
+
+    sock.SendMultipart([][]byte{[]byte("route"), data}, 0)
+    sock.RecvMultipart(0)
+}
+
 func (router *Router) Lookup(path string) (host Host, err error) {
-	router.mu.RLock()
-	defer router.mu.RUnlock()
+    router.mu.RLock()
+    defer router.mu.RUnlock()
 
-	// Extract the prefix from the given path
-	split := strings.Split(path, "/")
-	if len(split) >= 2 {
-		prefix := split[1]
+    // Extract the prefix from the given path
+    split := strings.Split(path, "/")
+    if len(split) >= 2 {
+	prefix := split[1]
 
-        // Load routes from database
-        route := Route{}
-        router.routeStore.Get(&route, "SELECT endpoint FROM services WHERE name=$1", prefix)
+	// Load routes from database
+	//route := Route{}
+	redis_key := fmt.Sprintf("route:%s", prefix)
 
-        // Create route handler
-        url, _ := url.Parse(route.Endpoint)
+	// Create route handler
+	domain, _ := redis.String(router.store.Do("HGET", redis_key, "domain"))
+	label, _ := redis.String(router.store.Do("HGET", redis_key, "label"))
+	routeprefix, _ := redis.String(router.store.Do("HGET", redis_key, "prefix"))
+	path, _ := redis.String(router.store.Do("HGET", redis_key, "path"))
 
-        domain := url.Host
-        label := prefix
+	url, _ := url.Parse(path)
 
         proxy := httputil.NewSingleHostReverseProxy(url)
-        routeprefix := "/" + label
-        path := url.String()
 
-        router.Register(label, domain, path, routeprefix, proxy)
+	router.Register(label, domain, path, routeprefix, proxy)
 
-        host = router.Hosts[prefix]
-        return host, nil
-	}
+	host = router.Hosts[prefix]
+	return host, nil
+    }
 
-	err = errors.New("404 Not Found")
-	return Host{}, err
+    err = errors.New("404 Not Found")
+    return Host{}, err
 }
 
 func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -98,33 +136,35 @@ func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
     // Get private key
     var private_key string
-    router.keyStore.QueryRow("SELECT private_key FROM keystore WHERE public_key=?", f.Get("key")).Scan(&private_key)
-    fmt.Println(private_key)
+    redis_key := fmt.Sprintf("keys:%s", public_key)
+    store := router.store
+    private_key, _ = redis.String(store.Do("get", redis_key))
+    /*  router.keyStore.QueryRow("SELECT private_key FROM keystore WHERE public_key=?", f.Get("key")).Scan(&private_key) */
 
     valid := Authenticate(digest, public_key, private_key, now, path, method)
     // authorized := Authorize()
     if valid != true {
-        w.WriteHeader(http.StatusUnauthorized)
-		return
+	w.WriteHeader(http.StatusUnauthorized)
+	return
     }
 
-	// Fetch host by the given path
-	host, err := router.Lookup(r.URL.Path)
-	if err != nil {
-	    http.NotFound(w, r)
-		return
-	}
+    // Fetch host by the given path
+    host, err := router.Lookup(r.URL.Path)
+    if err != nil {
+	http.NotFound(w, r)
+	return
+    }
 
-	// Build new path removing prefix
-	split := strings.Split(r.URL.Path, "/")
-	r.URL.Path = "/" + strings.Join(split[2:], "/")
+    // Build new path removing prefix
+    split := strings.Split(r.URL.Path, "/")
+    r.URL.Path = "/" + strings.Join(split[2:], "/")
 
-	// Assign target host header
-	r.Host = host.Domain
+    // Assign target host header
+    r.Host = host.Domain
 
-	// Assign handler
-	handler := host.handler
+    // Assign handler
+    handler := host.handler
 
-	// Serve request
-	handler.ServeHTTP(w, r)
+    // Serve request
+    handler.ServeHTTP(w, r)
 }
